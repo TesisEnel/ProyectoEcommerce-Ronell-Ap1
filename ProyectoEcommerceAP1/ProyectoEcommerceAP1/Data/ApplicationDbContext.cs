ca28710aaed2e166c2887d768884887fa73efbbf
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-13 19:56:40 -0400

    Add project files.

---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoEcommerceAP1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
