273e57fee2477e2e9e23af0e47cb02cc69801cff
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-16 22:00:18 -0400

    Todos los controladores, categorias, clientes, productos y telefonosClientes

---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Controllers/CategoriasController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoEcommerceAP1.Data;
+using Shared.Models;
+
+namespace ProyectoEcommerceAP1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categorias>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        // GET: api/Categorias/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categorias>> GetCategorias(int id)
+        {
+            var categorias = await _context.Categorias
+                                .Where(ca => ca.CategoriaId == id)
+                                .FirstOrDefaultAsync();
+
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+
+            return categorias;
+        }
+
+        // PUT: api/Categorias/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategorias(int id, Categorias categorias)
+        {
+            if (id != categorias.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categorias).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriasExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categorias
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Categorias>> PostCategorias(Categorias categorias)
+        {
+            if(!CategoriasExists(categorias.CategoriaId))
+            {
+                _context.Categorias.Add(categorias);
+            }
+            else
+            {
+                _context.Categorias.Update(categorias);
+            }
+
+            await _context.SaveChangesAsync();
+            return Ok(categorias);
+        }
+
+        // DELETE: api/Categorias/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategorias(int id)
+        {
+            var categorias = await _context.Categorias.FindAsync(id);
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categorias);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoriasExists(int id)
+        {
+            return _context.Categorias.Any(e => e.CategoriaId == id);
+        }
+    }
+}
---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Controllers/TelefonosClientesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoEcommerceAP1.Data;
+using Shared.Models;
+
+namespace ProyectoEcommerceAP1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelefonosClientesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TelefonosClientesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TelefonosClientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TelefonosClientes>>> GetTelefonosClientes()
+        {
+            if(_context.TelefonosClientes == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.TelefonosClientes.ToListAsync();
+        }
+
+        // GET: api/TelefonosClientes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TelefonosClientes>> GetTelefonosClientes(int id)
+        {
+            var telefonosClientes = await _context.TelefonosClientes.FindAsync(id);
+
+            if (telefonosClientes == null)
+            {
+                return NotFound();
+            }
+
+            return telefonosClientes!;
+        }
+    }
+}
