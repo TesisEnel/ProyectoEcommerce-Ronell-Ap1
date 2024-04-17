927b373093adc7e0221325380d23143e51a376e7
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-16 21:56:50 -0400

    Agregando los modelos en el DbContext

--- ProyectoEcommerceAP1/ProyectoEcommerceAP1/Data/ApplicationDbContext.cs
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Data/ApplicationDbContext.cs
@@ -1,9 +1,47 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace ProyectoEcommerceAP1.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Clientes> Clientes { get; set;}
+
+        public DbSet<TelefonosClientes> TelefonosClientes { get; set; } = default!;
+
+        public DbSet<Productos> Productos { get; set; }
+
+        public DbSet<Categorias> Categorias { get; set; }
+
+
+
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<TelefonosClientes>().HasData(new List<TelefonosClientes>()
+            {
+                new TelefonosClientes(){
+                    TipoTelId = 1, TipoTelefonos ="Telefono Local"
+                },
+
+                new TelefonosClientes(){
+                    TipoTelId = 2, TipoTelefonos = "Celular"
+                },
+
+                new TelefonosClientes(){
+                    TipoTelId = 3, TipoTelefonos = "Trabajo"
+                },
+
+                new TelefonosClientes(){
+                    TipoTelId = 4, TipoTelefonos ="Persona Auxiliar"
+                }
+            });
+        }
     }
 }
