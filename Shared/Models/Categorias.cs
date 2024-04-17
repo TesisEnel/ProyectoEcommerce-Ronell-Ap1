c4c4c9936582b17054f45f3fa258283ddf8886fe
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-16 21:55:43 -0400

    Creando el modelo de productos y el modelo de categorias

---
+++ Shared/Models/Categorias.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Categorias
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string NombreCategoria { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Url { get; set; }
+    }
+}
---
+++ Shared/Models/Productos.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [ForeignKey("CategoriaId")]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Descripcion { get; set; } = string.Empty;
+
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public DateTime FechaEntrada { get; set; } = DateTime.Now;
+
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public float Precio { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int CantidadProducto { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio"), DisplayName("Imagen del producto")]
+        public string? ImagenProducto { get; set; }
+
+        public float PrecioOferta {  get; set; }
+
+        public bool EnCarrito { get; set; } = false;
+
+
+    }
+}
