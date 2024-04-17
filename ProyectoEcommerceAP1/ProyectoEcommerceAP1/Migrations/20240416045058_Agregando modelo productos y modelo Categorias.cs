b3b2247d33bb7b1d168116e849fbed5d47c5df86
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-16 21:58:07 -0400

    Migracion de cuando se crearon productos y categorias

---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Migrations/20240416045058_Agregando modelo productos y modelo Categorias.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoEcommerceAP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandomodeloproductosymodeloCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Existencia",
+                table: "Productos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagenProducto",
+                table: "Productos",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaEntrada",
+                table: "Productos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaEntrada",
+                table: "Productos");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImagenProducto",
+                table: "Productos",
+                type: "BLOB",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Existencia",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
