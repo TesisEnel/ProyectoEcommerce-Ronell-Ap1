f4515d31882c73899ad5a45addb467b2fa339584
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-18 18:08:56 -0400

    Agregando los modelos Carrito e ItemCarrito

---
+++ Shared/Models/Carrito.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Carrito
+    {
+        [Key]
+        public int CarritoId { get; set; }
+
+        [Required]
+        public int ClienteId {  get; set; }
+
+        [ForeignKey("ClienteId")]
+        public Clientes Cliente { get; set; }
+
+        public ICollection<ItemCarrito> Items { get; set; } = new List<ItemCarrito>();
+
+        public float Total { get; set; }
+    }
+}
---
+++ Shared/Models/ItemCarrito.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class ItemCarrito
+    {
+        [Key]
+        public int ItemCarritoId { get; set; }
+
+        public int ProductoId { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public Productos Producto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public float SubTotal {  get; set; }
+    }
+}
