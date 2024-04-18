cd56bd609c9ba8e45388f0217a8559076b1e15ff
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-18 18:09:21 -0400

    Agregando la migracion de modelos Carrito e ItemCarrito

---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Migrations/20240417235003_Agregando modelo Carrito e ItemCarrito.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoEcommerceAP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandomodeloCarritoeItemCarrito : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Carrito",
+                columns: table => new
+                {
+                    CarritoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Total = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carrito", x => x.CarritoId);
+                    table.ForeignKey(
+                        name: "FK_Carrito_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "ClienteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemsCarrito",
+                columns: table => new
+                {
+                    ItemCarritoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubTotal = table.Column<float>(type: "REAL", nullable: false),
+                    CarritoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemsCarrito", x => x.ItemCarritoId);
+                    table.ForeignKey(
+                        name: "FK_ItemsCarrito_Carrito_CarritoId",
+                        column: x => x.CarritoId,
+                        principalTable: "Carrito",
+                        principalColumn: "CarritoId");
+                    table.ForeignKey(
+                        name: "FK_ItemsCarrito_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Carrito_ClienteId",
+                table: "Carrito",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemsCarrito_CarritoId",
+                table: "ItemsCarrito",
+                column: "CarritoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemsCarrito_ProductoId",
+                table: "ItemsCarrito",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemsCarrito");
+
+            migrationBuilder.DropTable(
+                name: "Carrito");
+        }
+    }
+}
