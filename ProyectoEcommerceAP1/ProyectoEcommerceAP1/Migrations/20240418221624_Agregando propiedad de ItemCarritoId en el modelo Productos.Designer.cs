3436745708588603a5219d8ff2f81cdaecacc263
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-19 10:31:54 -0400

    Agregando la migracion de la modificacion del modelo productos

--- ProyectoEcommerceAP1/ProyectoEcommerceAP1/Migrations/20240418221624_Agregando propiedad de ItemCarritoId en el modelo Productos.Designer.cs
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Migrations/20240418221624_Agregando propiedad de ItemCarritoId en el modelo Productos.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoEcommerceAP1.Data;
 
@@ -10,9 +11,11 @@ using ProyectoEcommerceAP1.Data;
 namespace ProyectoEcommerceAP1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240418221624_Agregando propiedad de ItemCarritoId en el modelo Productos")]
+    partial class AgregandopropiedaddeItemCarritoIdenelmodeloProductos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
---
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Migrations/20240418221624_Agregando propiedad de ItemCarritoId en el modelo Productos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoEcommerceAP1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandopropiedaddeItemCarritoIdenelmodeloProductos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemCarritoId",
+                table: "Productos",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemCarritoId",
+                table: "Productos");
+        }
+    }
+}
