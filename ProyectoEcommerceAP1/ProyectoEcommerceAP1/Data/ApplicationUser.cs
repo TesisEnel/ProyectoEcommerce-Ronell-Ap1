aa1e3fa2e347722c3edb434290341591502a1d31
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2024-04-22 18:55:16 -0400

    Agregando propiedades basicas para el usuario

--- ProyectoEcommerceAP1/ProyectoEcommerceAP1/Data/ApplicationUser.cs
+++ ProyectoEcommerceAP1/ProyectoEcommerceAP1/Data/ApplicationUser.cs
@@ -1,10 +1,21 @@
 using Microsoft.AspNetCore.Identity;
+using Shared.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace ProyectoEcommerceAP1.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string NombreCliente { get; set; } = string.Empty;
+
+        public string ApellidoCliente {  get; set; } = string.Empty;
+
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now.AddYears(-20);
+
+        public string NumeroCedula { get; set; } = string.Empty;
+
     }
 
 }
